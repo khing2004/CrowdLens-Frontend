0d1060ac4cee1edf5a08385c0de6eb66b6d4bb7d
SavvyAvie <36625433+savvyavie@users.noreply.github.com>
2026-03-21 16:09:06 +0800

    changed starting page to Login

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/forecast" element={<Forecast />} />
         <Route path="/home" element={<UserHomePage />} />
         <Route path="/settings" element={<Settings />} />
