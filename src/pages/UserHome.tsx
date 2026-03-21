e4804ab7c591f5c3e815141118895d40c0df489b
khing2001 <caritero.khingiankeith1@gmail.com>
2026-03-21 11:54:40 +0800

    [View Crowd Level] Added popups and minor functions such as automatic recenter and dynamic color change for badge

--- src/pages/UserHome.tsx
+++ src/pages/UserHome.tsx
@@ -4,6 +4,22 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./UserHome.css";
 import { Link } from "react-router-dom";
+import AreaModal from "../components/AreaModal"; // components folder has reusable components which can be used at any page.
+import { useState, useEffect } from "react";
+
+// Helper component to handle programmatic panning
+function RecenterAutomatically({ location }: { location: any }) {
+  const map = useMap();
+  useEffect(() => {
+    if (location) {
+      map.flyTo(location.pos, 16, {
+        animate: true,
+        duration: 0.7, // Smooth pan duration in seconds
+      });
+    }
+  }, [location, map]);  
+  return null;
+}
 
 // for pulse marker
 const createPulseIcon = (color: string) => {
@@ -15,16 +31,50 @@ const createPulseIcon = (color: string) => {
   });
 };
 
+const densityClasses: Record<string, string> = {
+  "High": "high",
+  "Medium": "medium",
+  "Low": "low"
+};
+
 export default function UserHomePage() {
 
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState<any>(null);
+
+
+  const getIconByDensity = (density: string) => {
+    if (density == "High") return createPulseIcon("#d32f2f");
+    if (density == "Medium") return createPulseIcon("#f57c00");
+    return createPulseIcon("#388e3c");
+
+  }
   // Center of Cebu 
   const center: [number, number] = [10.3223, 123.8982];
 
   //Mock data for Areas (replace with api call later)
-  const areas = [
-    { id: 1, name: "IT Park", pos: [10.3280, 123.9055], density: "High", color: "#d32f2f" },
-    { id: 2, name: "Fuente Circle", pos: [10.3103, 123.8935], density: "Low", color: "#388e3c" }
-  ];
+  const locations = [
+  { 
+    id: 1, 
+    name: "Cebu City Public Library", 
+    type: "Public Library",
+    pos: [10.3095, 123.8931], 
+    density: "Medium", 
+    lastUpdated: "5 mins ago" 
+    
+  },
+  { 
+    id: 2, 
+    name: "Vicente Sotto Medical Center", 
+    type: "Hospital",
+    pos: [10.3117, 123.8915], 
+    density: "High", 
+    lastUpdated: "2 mins ago" 
+  }
+
+  
+];
 
   return (
     <div className="user-home-page">
@@ -53,15 +103,45 @@ export default function UserHomePage() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // Clean grey map style
             attribution='&copy; OpenStreetMap'
           />
-          {areas.map(area => (
+
+          {selectedLocation && (
+            <RecenterAutomatically 
+              location ={selectedLocation} 
+            />
+          )}
+
+          {locations.map(location => (
             <Marker 
-              key={area.id} 
-              position={area.pos as [number, number]} 
-              icon={createPulseIcon(area.color)}
+              key={location.id} 
+              position={location.pos as [number, number]} 
+              icon={getIconByDensity(location.density)}
+              eventHandlers={{
+                click: () => setSelectedLocation(location)
+              }}
             >
-              <Popup>
-                <strong>{area.name}</strong><br/>
-                Status: {area.density} Density
+              <Popup className="custom-popup">
+                <div className="popup-container">
+                  <div className="popup-header">
+                    <div className="title-row">
+                      <span className="star-icon">☆</span>
+                      <h2>{location.name}</h2>
+                    </div>
+                    <div className="status-row">
+                      <span className={`badge ${densityClasses[location.density]}`}>● {location.density} Crowd Level</span>
+                      <span className="updated-text">    Updated 2 minutes ago.</span>
+                    </div>
+                    <p className="quieter-nearby">Quieter Nearby: Lahug Area (Light Crowd)</p>
+                  </div>
+
+                  <div className="congestion-info">
+                    <h3>Peak Lunchtime Congestion</h3>
+                    <p>Most establishments are busy. Expect 10-20 minute waits and limited seating availability.</p>
+                  </div>
+
+                  <button className="input-btn">
+                    <span className="plus-icon">+</span> Input Crowd Level
+                  </button>
+                </div>
               </Popup>
             </Marker>
           ))}
